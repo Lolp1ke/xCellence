c58e96feebe5eb2934dac3d155450e0c1e09d55c
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-11-07 01:25:55 +0600

    i hope this works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Autonomous", group = "xCellence")
+public class autonomous extends LinearOpMode {
+    @Override()
+    public void runOpMode() {
+    }
+}
