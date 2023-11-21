f7b23d9e5c2b30ecdb1e521c616201d58fca548e
Alibek Bekzhanov <freed.alibek@gmail.com>
2023-11-21 18:51:19 +0600

    config fixes and stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism.java
@@ -17,8 +17,7 @@ public class mechanism {
     private Servo right_claw;
     private Servo left_claw;
 
-    private double handPosition = 1.0d;
-    private boolean clawClosed = false;
+    private double handPosition = _config.HAND_GROUND;
 
     public mechanism(LinearOpMode _opMode) {
         opmode = _opMode;
@@ -29,11 +28,9 @@ public class mechanism {
         double avgPosition = (right_arm.getCurrentPosition() + left_arm.getCurrentPosition()) / 2.0d;
 
         if (opmode.gamepad2.x) {
-            handPosition = 1.0d;
+            handPosition = _config.HAND_CLOSE;
         } else if (opmode.gamepad2.a) {
-            handPosition = 0.3d;
-        } else if (opmode.gamepad2.b) {
-            handPosition = 0.0d;
+            handPosition = _config.HAND_GROUND;
         }
 
         if (opmode.gamepad2.right_bumper) {
@@ -42,21 +39,29 @@ public class mechanism {
             armPower = -0.1d;
         }
 
-        if (avgPosition < -15) {
-            right_arm.setPower(armPower);
-            left_arm.setPower(armPower);
-        } else {
-            right_arm.setPower(-.3d);
-            left_arm.setPower(-.3d);
-        }
+//        if (avgPosition < -5) {
+        right_arm.setPower(armPower);
+        left_arm.setPower(armPower);
+//        } else {
+//            right_arm.setPower(-0.3d);
+//            left_arm.setPower(-0.3d);
+//        }
 
         hand.setPosition(handPosition);
+        right_claw.setPosition(opmode.gamepad2.y ? _config.CLAW_CLOSE : opmode.gamepad2.dpad_right ? _config.CLAW_CLOSE : _config.CLAW_OPEN);
+        left_claw.setPosition(opmode.gamepad2.y ? _config.CLAW_CLOSE : opmode.gamepad2.dpad_left ? _config.CLAW_CLOSE : _config.CLAW_OPEN);
 
-        right_claw.setPosition(opmode.gamepad2.y ? 0.5d : (opmode.gamepad2.dpad_up ? .5d : 0.0d));
-        left_claw.setPosition(opmode.gamepad2.y ? 0.5d : (opmode.gamepad2.dpad_down ? .5d : 0.0d));
+//        right_claw.setPosition(opmode.gamepad2.y ?
+//                1.0d : (opmode.gamepad2.dpad_up ? 1.0d : 0.3d));
+//
+//        left_claw.setPosition(opmode.gamepad2.y ?
+//                0 : (opmode.gamepad2.dpad_up ? 0.3d : 1.0d));
 
         opmode.telemetry.addData("Arm: ", armPower);
         opmode.telemetry.addData("Hand: ", handPosition);
+        opmode.telemetry.addData("Claw: ", opmode.gamepad2.y);
+        opmode.telemetry.addData("Right claw: ", opmode.gamepad2.dpad_up);
+        opmode.telemetry.addData("Left claw: ", opmode.gamepad2.dpad_down);
 
         opmode.telemetry.addLine("Dev:");
         opmode.telemetry.addData("Average arm position: ", avgPosition);
@@ -73,8 +78,11 @@ public class mechanism {
         right_arm.setDirection(DcMotorSimple.Direction.FORWARD);
         left_arm.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        right_claw.setDirection(Servo.Direction.FORWARD);
+        left_claw.setDirection(Servo.Direction.REVERSE);
+
         hand.setPosition(handPosition);
-        right_claw.setPosition(0.0d);
-        left_claw.setPosition(0.0d);
+        right_claw.setPosition(_config.CLAW_OPEN);
+        left_claw.setPosition(_config.CLAW_OPEN);
     }
 }
